b35f1c45f24b4b75c89ff15c5bbf6c13489835b8
Reshhh1 <reshwanbarhoe1@gmail.com>
2023-03-22 23:36:42 +0100

    first commit

---
+++ src/app/components/buttons/buttons.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons',
+  templateUrl: './buttons.component.html',
+  styleUrls: ['./buttons.component.css']
+})
+export class ButtonsComponent implements OnInit {
+  @Input() text!: string ;
+  @Input() color!: string;
+
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
+}
